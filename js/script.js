e2fbf90143fab23dc7436211b7435661ac798420
Angelina Vaganova <72302734+linenakk@users.noreply.github.com>
2022-01-28 00:28:27 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+console.log('Hello'+' world'); // Конкотенация (Слепить строки)
+console.info('Hello'+' world'); // тоже самое
+//alert('всплывающее окно')
+document.getElementById('out').innerHTML = 'Art'; // innerHTML-присвоение
+
+document.querySelector('.head').innerHTML = 'замена классов';  // более новая ве6рсия. заменяет первый встречный селектор. Эта инструкция более гибкая
+document.querySelector('#one').innerHTML = 'Замена id'// тожесамое и с getElementById, но без #
+
+let b; //Лет- это объявление переменной. 2 раза одну переменную объявлять нельзя
+let a = document.querySelector('#one'); //внутрь а получил параграф.
+
+
+let c; //можно сначала объявить
+c = document.querySelector('.head'); //потом присвоить
+a.innerHTML=999;
+c.innerHTML=999;
